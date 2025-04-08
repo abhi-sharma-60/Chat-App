36678ac5373ac21007dab1de691a9eac61fbe8b9
Crisonel <adityapratap1703@gmail.com>
2025-04-08 15:21:54 +0530

    Add Search User Api

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -5,6 +5,7 @@ import checkPassword from "../controllers/checkPassword.js"
 import userDetails from "../controllers/userDetails.js"
 import logout from "../controllers/logout.js"
 import updateUserDetails from "../controllers/updateUserDetails.js"
+import searchUser from "../controllers/searchUser.js"
 
 const router = express.Router()
 
@@ -12,7 +13,9 @@ router.post("/register",registerUser)
 router.post("/email",checkEmail)
 router.post("/password",checkPassword)
 router.get("/user-details",userDetails)
-router.post("/logout",logout)
+router.get("/logout",logout)
 router.post("/update-user",updateUserDetails)
+router.post("/search-user",searchUser)
+
 
 export default router
